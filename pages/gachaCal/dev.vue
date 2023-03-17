d57e490404002b33fc7d7d77de58efc13a2327f8
sakura <1969314158@qq.com>
2023-03-17 10:40:11 +0800

    修复攒抽计算器不能输入负数的bug

--- pages/gachaCal/dev.vue
+++ pages/gachaCal/dev.vue
@@ -212,12 +212,12 @@
           <el-divider></el-divider>
           <!-- 自定义修正值 -->
           <div class="gacha_unit_child">
-            <input
+           <input
               class="gacha_unit_child_inputbox"
               type="text"
               @change="compute()"
               v-model.number="customValue"
-              oninput="value=value.replace(/[^\d]/g, '')"
+              oninput="value=value.replace(/[^0-9-]+/g,'~')"
             />
             <div class="gacha_unit_child_title" style="width: 330px">
               自定义修正值(合成玉)
@@ -266,6 +266,7 @@
               type="text"
               @change="compute()"
               v-model.number="orundum_ap"
+              oninput="value=value.replace(/[^0-9-]+/g, '')"
             />
             用于搓玉的理智 X
             <input
